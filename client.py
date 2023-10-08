18a0a5876e7bbac9fd4a089fa70d9c91e49fc895
Guillermo San Martín <guille.seba0802@gmail.com>
2023-10-08 20:53:24 -0300

    utf-8

--- client.py
+++ client.py
@@ -10,9 +10,9 @@ client.connect(('127.0.0.1', 55555))
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
+            message = client.recv(1024).decode('utf-8')
             if message == 'NICK':
-                client.send(nickname.encode('ascii'))
+                client.send(nickname.encode('utf-8'))
             else:
                 print(message)
         except:
@@ -24,7 +24,7 @@ def receive():
 def write():
     while True:
         message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+        client.send(message.encode('utf-8'))
 
 
 receive_thread = threading.Thread(target=receive)
